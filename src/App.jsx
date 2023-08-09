503a7736aec8a0ae1dc1c739b2f6f5105eede51f
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2023-08-09 11:49:55 +0200

    Poke api fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,46 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import NitendoSwitch from "./components/NintendoSwitch";
 import Home from "./components/Home";
 import PokeList from "./components/PokeList";
 import PokeDex from "./components/PokeDex";
-import NitendoSwitch from "./components/NintendoSwitch";
 import PokeFight from "./components/PokeFight";
 
 function App() {
+  // const [loading, setLoading] = useState(true);
+  const [pokemons, setPokemons] = useState([]);
+
+  const [currentUrl, setCurrentUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=151"
+  );
+  const [nextUrl, setNextUrl] = useState();
+  const [previousUrl, setpreviousUrl] = useState();
+
+  // console.log(pokemons && pokemons.allData);
+
+  useEffect(() => {
+    axios.get(currentUrl).then((response) => {
+      Promise.all(
+        response.data.results.map((pokemon) => axios.get(pokemon.url))
+      ).then(
+        axios.spread((...allData) => {
+          setPokemons({ allData });
+        })
+      );
+    });
+  }, [currentUrl]);
+
   return (
     <>
       <NitendoSwitch />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/pokemon" element={<PokeList />} />
+        <Route
+          path="/pokemon"
+          element={<PokeList pokemons={pokemons.allData} />}
+        />
         <Route path="/pokemon/:id" element={<PokeDex />} />
         <Route path="/pokemon/fight" element={<PokeFight />} />
       </Routes>
--- src/components/PokeList.jsx
+++ src/components/PokeList.jsx
@@ -1,7 +1,19 @@
-const PokeList = () => {
+const PokeList = ({ pokemons }) => {
+  if (pokemons) {
+    console.log("pokeList:", pokemons);
+  }
+
   return (
     <>
       <h1>Hallo PokeList</h1>
+      {pokemons &&
+        pokemons.map((pokemon) => {
+          return (
+            <li key={pokemon.data.name}>
+              PokeID: {pokemon.data.id} Pokemon: {pokemon.data.name}
+            </li>
+          );
+        })}
     </>
   );
 };
