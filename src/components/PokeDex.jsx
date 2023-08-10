1e580b8f82c227fe3c1842afa82ec210947c6988
claudia-sikorski <c.sikorski@outlook.de>
2023-08-10 18:08:53 +0200

    add height and weight from api data

--- src/components/PokeDex.jsx
+++ src/components/PokeDex.jsx
@@ -1,5 +1,5 @@
 // Imports
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -12,28 +12,23 @@ import { PokeTypes } from "./PokeTypes.jsx";
 const PokeDex = () => {
   let { id } = useParams();
 
-  const [pokeData, setPokeData] = useState({});
-  console.log(pokeData);
-
+  // const [pokeData, setPokeData] = useState({});
   const [name, setName] = useState("");
   const [number, setNumber] = useState(null);
   const [types, setTypes] = useState([]);
-  console.log(types);
-
-  let pokeTypes = ["normal"];
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
 
   useEffect(() => {
     axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) => {
-      setPokeData(response.data);
+      // setPokeData(response.data);
       setName(response.data.name);
       setNumber(response.data.id);
-      // response.data.types.map((type) => {
-      //   return setTypes(type.name);
-      // }); FOR Loop?
+      setTypes(response.data.types.map((type) => type.type.name));
+      setHeight(response.data.height / 10);
+      setWeight(response.data.weight / 10);
     });
-  }, [id, setPokeData]);
-
-  console.log(pokeData);
+  }, [id]);
 
   return (
     <>
@@ -50,26 +45,24 @@ const PokeDex = () => {
           <h1>{name.charAt(0).toUpperCase() + name.slice(1)}</h1>
           <p>{`No. ${number}`}</p>
         </div>
-
         <div className="pokemonType_Container">
-          {pokeTypes.map((type) => {
+          {types.map((type) => {
             // console.log(type);
             return <PokeTypes key={type} type={type} />;
           })}
-
-          {/* <div className="pokemonType" style={{ backgroundColor: "#a8a899" }}>
-            <img src={Normal} alt="PokemonType" />
-            <p>Normal</p>
-          </div> */}
         </div>
-
         <div className="pokeDexImage">
           <img
             src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${number}.png`}
             alt="pokemon"
           />
         </div>
-        <div className="pokeDexDetails"></div>
+        <div className="pokeDexDetails">
+          <p>Height</p>
+          <p>{`${height} m`}</p>
+          <p>Weight</p>
+          <p>{`${weight} kg`}</p>
+        </div>
         <div className="pokeDexAttacks"></div>
         <div className="pokeDex_Footer"></div>
       </div>
@@ -78,12 +71,3 @@ const PokeDex = () => {
 };
 
 export default PokeDex;
-
-// Source: https://pokeapi.co/api/v2/pokemon/:id
-
-// Pokemon Name: pokemon.moves.name
-// Pokemon ID: pokemon.held_items.id
-// Pokemon Types: https://pokeapi.co/api/v2/type/
-// Pokemon Image: https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/:id.png
-// Pokemon Height in m -> if height.length <2 return .join (0 am Anfang) + zusammenfügen + join("."): pokemon.game_indices.height
-// Pokemon Weight in kg -> for-loop + return .join('.'): pokemon.types.weight
