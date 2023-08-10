08a69cffbb43aa899f58b5b6dee3e667e172053a
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2023-08-10 11:24:15 +0200

    display pokemons in PokeList component

--- src/components/PokeList.jsx
+++ src/components/PokeList.jsx
@@ -1,24 +1,37 @@
 import "./styles/pokeList.css";
+import { Link } from "react-router-dom";
 
 const PokeList = ({ pokemons }) => {
-  if (pokemons) {
-    console.log("pokeList:", pokemons);
-  }
+  // if (pokemons) {
+  //   console.log("pokeList:", pokemons);
+  // }
 
   return (
     <>
-      <h1>Hallo PokeList</h1>
-      {pokemons &&
-        pokemons.map((pokemon) => {
-          return (
-            <div className="poke-list-container" key={pokemon.data.name}>
-              <li>
-                PokeID: {pokemon.data.base_experience} Pokemon:{" "}
-                {pokemon.data.name}
-              </li>
-            </div>
-          );
-        })}
+      <div className="poke-list-container">
+        {pokemons &&
+          pokemons.map((pokemon) => {
+            const pokeName =
+              pokemon.data.name.charAt(0).toUpperCase() +
+              pokemon.data.name.slice(1);
+            return (
+              <Link
+                key={pokemon.data.name}
+                className="poke-list-link"
+                to={`pokemon/${pokemon.data.id}`}
+                pokeid={pokemon.data.id}
+              >
+                <img
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                    pokemon.data.id && pokemon.data.id
+                  }.png`}
+                  alt=""
+                />
+                <p>{pokeName}</p>
+              </Link>
+            );
+          })}
+      </div>
     </>
   );
 };
