1cf642417c5637ac150754406447e29d103d29a1
uaeboss <jeskemal@gmail.com>
2023-08-11 18:59:00 +0200

    added display pokemon after select

--- src/components/DropdownPokemon.jsx
+++ src/components/DropdownPokemon.jsx
@@ -1,17 +1,39 @@
 import "./styles/dropdown.css";
 import Select from "react-select";
+import { useState } from "react";
 
 const PokemonDropdown = ({ pokemons }) => {
+  const [userSelect, setUserSelect] = useState(null);
+
+  const onChangeHandler = (selectedOption) => {
+    setUserSelect(selectedOption.value);
+  };
+
+    console.log('userselect:', userSelect);
+  
   return (
     <>
+      {!userSelect &&
       <div className="dropdown_select">
-      {pokemons && 
-        <Select
-          options={pokemons.map((pokemon) => {
-            return { value: pokemon.data.name, label: pokemon.data.name.charAt(0).toUpperCase() + pokemon.data.name.slice(1) };
-          })}
-        />}
-      </div>
+        {pokemons && (
+          <Select
+          
+            onChange={onChangeHandler}
+            options={pokemons.map((pokemon) => {
+              return {
+                value: pokemon.data,
+                label:
+                  pokemon.data.name.charAt(0).toUpperCase() +
+                  pokemon.data.name.slice(1),
+              };
+            })}
+          />
+        )}
+        </div>
+        }
+        {userSelect && 
+        <img src={userSelect && userSelect.sprites.back_default} />
+      }
     </>
   );
 };
