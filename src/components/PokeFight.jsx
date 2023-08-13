b896de39097bd40781235b98a453913927304be6
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2023-08-13 16:50:42 +0200

    add function for computer attack

--- src/components/PokeFight.jsx
+++ src/components/PokeFight.jsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 
 const PokeFight = ({ pokemons }) => {
   const [enemyFightHp, setEnemyFightHp] = useState(100);
+  const [userFightHp, setUserFightHp] = useState(100);
   const [fightHp, setFightHp] = useState(100);
   const [userSelect, setUserSelect] = useState(null);
   const [selectPokemonMessage, setSelectPokemonMessage] = useState(false);
@@ -25,13 +26,24 @@ const PokeFight = ({ pokemons }) => {
   }
 
   const userAttack = userSelect && userSelect.stats[1].base_stat;
+  const userHp = userSelect && userSelect.stats[0].base_stat;
+
   const enemyHp = randomPokemon && randomPokemon.data.stats[0].base_stat;
+  const enemyAttack = randomPokemon && randomPokemon.data.stats[1].base_stat;
 
   function userFight() {
     if (!userSelect) {
       setSelectPokemonMessage(true);
     } else {
       setEnemyFightHp(enemyHp - userAttack / 3);
+    }
+  }
+
+  function computerFight() {
+    if (!userSelect) {
+      setSelectPokemonMessage(true);
+    } else {
+      setUserFightHp(userHp - enemyAttack / 3);
     }
   }
 
@@ -56,12 +68,16 @@ const PokeFight = ({ pokemons }) => {
             fight={fightHp}
             randomPokemon={randomPokemon}
             enemyFightHp={enemyFightHp}
+            userFightHp={userFightHp}
           />
         </div>
 
         <div className="pokefight_button">
           <button onClick={userFight} className="pokefight_fightbtn">
             Attack!
+          </button>
+          <button onClick={computerFight} className="pokefight_fightbtn">
+            ComputerAttack test!
           </button>
           <button className="pokefight_fightbtn " onClick={newGame}>
             New Game
