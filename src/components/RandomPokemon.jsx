6da715134a924658c57c651746ac53aae0dcea94
uaeboss <jeskemal@gmail.com>
2023-08-15 17:47:00 +0200

    fixed pokefight css

--- src/components/RandomPokemon.jsx
+++ src/components/RandomPokemon.jsx
@@ -3,11 +3,12 @@ import PokeFightHpBar from "./PokeFightHpBar";
 function RandomPokemon({ pokemon, enemyFightHp }) {
   return (
     <div className="pokefight_random_pokemon">
+      <PokeFightHpBar fight={enemyFightHp} />
       <img
         className="pokefight_random_pokemon_img"
         src={pokemon && pokemon.data.sprites.front_default}
       />
-      <PokeFightHpBar fight={enemyFightHp} />
+      
     </div>
   );
 }
