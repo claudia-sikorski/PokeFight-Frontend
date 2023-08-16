fd2583031edfdc4e8974f1da4bfdee419503d25d
claudia-sikorski <c.sikorski@outlook.de>
2023-08-16 12:31:46 +0200

    add tabs to pokedex

--- src/components/PokeDex.jsx
+++ src/components/PokeDex.jsx
@@ -1,32 +1,38 @@
 // Imports
+import axios from "axios";
 import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "./styles/PokeDex.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faAngleDoubleLeft,
   faAngleDoubleRight,
+  faGlobe,
+  faInfoCircle,
+  faExplosion,
 } from "@fortawesome/free-solid-svg-icons";
-
-import axios from "axios";
-
-import "./styles/PokeDex.css";
 import iconPokedex from "../assets/pokedex-icon.png";
-
+import iconHeight from "../assets/icon-height.png";
+import iconWeight from "../assets/icon-weight.png";
 import { PokeTypes } from "./PokeTypes.jsx";
 
 // PokeDex Component
 const PokeDex = () => {
   let { id } = useParams();
 
-  // const [pokeData, setPokeData] = useState({});
   const [name, setName] = useState("");
   const [number, setNumber] = useState(null);
   const [types, setTypes] = useState([]);
   const [height, setHeight] = useState("");
   const [weight, setWeight] = useState("");
   const [moves, setMoves] = useState([]);
+  const [hp, setHp] = useState(null);
+  const [attack, setAttack] = useState(null);
+  const [defense, setDefense] = useState(null);
+  const [speed, setSpeed] = useState(null);
 
-  let nextPokemon = number + 1;
+  const nextPokemon = number + 1;
   const previousPokemon = number - 1;
 
   useEffect(() => {
@@ -38,6 +44,10 @@ const PokeDex = () => {
       setHeight(response.data.height / 10);
       setWeight(response.data.weight / 10);
       setMoves(response.data.moves.map((move) => move.move.name));
+      setHp(response.data.stats[0].base_stat);
+      setAttack(response.data.stats[1].base_stat);
+      setDefense(response.data.stats[2].base_stat);
+      setSpeed(response.data.stats[5].base_stat);
     });
   }, [id]);
 
@@ -71,12 +81,80 @@ const PokeDex = () => {
           />
         </div>
         <div className="pokeDexDetails">
-          <p>Height</p>
-          <p>{`${height} m`}</p>
-          <p>Weight</p>
-          <p>{`${weight} kg`}</p>
+          <Tabs>
+            <TabList>
+              <Tab>
+                <FontAwesomeIcon className="Tab_Icons" icon={faInfoCircle} />
+              </Tab>
+              <Tab>
+                <FontAwesomeIcon className="Tab_Icons" icon={faGlobe} />
+              </Tab>
+              <Tab>
+                <FontAwesomeIcon className="Tab_Icons" icon={faExplosion} />
+              </Tab>
+            </TabList>
+
+            <TabPanel className="Panel_DetailsWrapper">
+              <div className="Panel_Details">
+                <img
+                  src={iconHeight}
+                  alt="Icon Height"
+                  className="Panel_Details_Image"
+                />
+                <div className="Panel_Details_Text">
+                  <p>Height</p>
+                  <p>{`${height} m`}</p>
+                </div>
+              </div>
+              <div className="Panel_Details">
+                <img
+                  src={iconWeight}
+                  alt="Icon Weight"
+                  className="Panel_Details_Image"
+                />
+                <div className="Panel_Details_Text">
+                  <p>Weight</p>
+                  <p>{`${weight} kg`}</p>
+                </div>
+              </div>
+            </TabPanel>
+            <TabPanel className="Panel_Stats">
+              <p id="stats_Header">Stats</p>
+              <div className="Panel_Stats_Wrapper">
+                <div className="Panel_Stats_Info1">
+                  <p>HP</p>
+                  <p>Attack</p>
+                  <p>Defense</p>
+                  <p>Speed</p>
+                </div>
+                <div className="Panel_Stats_Info2">
+                  <p>{hp}</p>
+                  <p>{attack}</p>
+                  <p>{defense}</p>
+                  <p>{speed}</p>
+                </div>
+              </div>
+            </TabPanel>
+            <TabPanel className="Panel_LearnedAttacks">
+              <p id="attack_Header">Learned Attacks</p>
+              <div className="ListedAttacks">
+                <p className="attack">
+                  {moves[Math.floor(Math.random() * moves.length)]}
+                </p>
+                <p className="attack">
+                  {moves[Math.floor(Math.random() * moves.length)]}
+                </p>
+                <p className="attack">
+                  {moves[Math.floor(Math.random() * moves.length)]}
+                </p>
+                <p className="attack">
+                  {moves[Math.floor(Math.random() * moves.length)]}
+                </p>
+              </div>
+            </TabPanel>
+          </Tabs>
         </div>
-        <div className="pokeDex_Attacks">
+        {/* <div className="pokeDex_Attacks">
           <p id="attack_Header">Learned Attacks</p>
           <p className="attack">
             {moves[Math.floor(Math.random() * moves.length)]}
@@ -90,7 +168,7 @@ const PokeDex = () => {
           <p className="attack">
             {moves[Math.floor(Math.random() * moves.length)]}
           </p>
-        </div>
+        </div> */}
         <div className="pokeDex_Footer">
           <Link
             to={`/pokemon/${previousPokemon}`}
