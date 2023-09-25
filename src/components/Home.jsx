e9253e75a3f07cbbc8559331a40dcdbf8f7d7235
claudia-sikorski <c.sikorski@outlook.de>
2023-09-25 14:58:16 +0200

    change other image imports in home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,10 @@ import { Link } from "react-router-dom";
 import "./styles/home.css";
 import { useState } from "react";
 import pokemonLogo from "../../public/home-img/pokemon-logo.png";
+import pokeball from "../../public/home-img/pokeball.png";
+import pokedex from "../../public/home-img/icons8-pokedex-80.png";
+import pokefight from "../../public/home-img/valor.png";
+// import highscores from "../../public/home-img/highscore.png";
 
 const Home = ({ playSong }) => {
   const [start, setStart] = useState(false);
@@ -20,11 +24,7 @@ const Home = ({ playSong }) => {
             {!start && (
               <Link onClick={showHome} to={"/"} className="poke-home-link">
                 <div className="poke-home-header">
-                  <img
-                    src="./public/home-img/pokeball.png"
-                    alt="Pokedex"
-                    className="menu-button"
-                  />
+                  <img src={pokeball} alt="Pokedex" className="menu-button" />
                 </div>
                 <p className="elt1">Start</p>
               </Link>
@@ -33,28 +33,20 @@ const Home = ({ playSong }) => {
               <>
                 <Link to={"/pokemon"} className="poke-home-link">
                   <div className="poke-home-header">
-                    <img
-                      src="./public/home-img/icons8-pokedex-80.png"
-                      alt="Pokedex"
-                      className="menu-button"
-                    />
+                    <img src={pokedex} alt="Pokedex" className="menu-button" />
                   </div>
                   <p className="elt1">POKEMON</p>
                 </Link>
                 <Link to={"/pokemon/fight"} className="poke-home-link">
                   <div className="poke-home-header">
-                    <img
-                      src="./public/home-img/valor.png"
-                      alt="Fght"
-                      className="menu-button"
-                    />
+                    <img src={pokefight} alt="Fght" className="menu-button" />
                   </div>
                   <p className="elt1">POKEFIGHT</p>
                 </Link>
                 {/* <Link className="poke-home-link">
                   <div className="poke-list-header">
                     <img
-                      src="./public/home-img/highscore.png"
+                      src={highscores}
                       alt=""
                       className="menu-button"
                     />
